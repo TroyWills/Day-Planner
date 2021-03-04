8dbb0d064b6dcbd76ade959e768ac077297dfcc4
Troy Wills <you@example.com>
2021-03-03 21:19:21 -0600

    progress on variables & local storage

--- Develop/script.js
+++ Develop/script.js
@@ -9,3 +9,14 @@ function displayTime() {
 // window.onload = displayTime;
 
 var 8AM = [];
+
+// saveButton on click event to store calendar time and event into local storage 
+$(".saveButton").on("click", function() {
+
+    // setting the variables for time and event from save button 
+    val event = $(this).("eventInput").val().trim();
+    val time - $(this).("timeSaved").val().trim();
+
+    // storing event & time in local storage
+    localStorage.setItem(event, time);
+  });
