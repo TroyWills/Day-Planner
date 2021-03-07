b13e32b0aa7523b8cbafd00f531a07a3292a72d5
Troy Wills <troy_wills@outlook.com>
2021-03-06 23:38:19 -0600

    local storage working

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-var currentDate = moment();
-console.log(currentDate);
+// var currentDate = moment();
+// console.log(currentDate);
 
-
-function displayTime() {
-    document.write(currentDate);
-}
-
-// window.onload = displayTime;
-
-var 8AM = [];
+// function displayTime() {
+//     document.write(currentDate);
+// }
 
 // saveButton on click event to store calendar time and event into local storage 
 $(".saveButton").on("click", function() {
 
-    // setting the variables for time and event from save button 
-    val event = $(this).("eventInput").val().trim();
-    val time - $(this).("timeSaved").val().trim();
+// putting taskinput and time into variables to later save in local storage
+    var time = $(this).parent().attr("id")
+    var taskInput = ($(this).siblings("#eventInput").val().trim());
+    console.log(time);
+    console.log(taskInput);
 
-    // storing event & time in local storage
-    localStorage.setItem(event, time);
-  });
+    // saving user input and time into local storage
+    localStorage.setItem(time, taskInput);
+});
